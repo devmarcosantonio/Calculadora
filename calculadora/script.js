b44e935d59695952b55a245ba5ce176ab7fede4f
Marcos Antonio <marcosjr2001dev@gmail.com>
2023-06-03 18:51:04 -0300

    Adicionei lógica AC de Del
    adicionei as lógicas de deletar e limpar tudo do visor.

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,6 +1,15 @@
 const visor = document.querySelector('#visor')
 
 function onClick(btn) {
-    console.log(btn)
-    visor.innerText += btn.innerText
+    switch (btn.innerText) {
+        case 'AC':
+            visor.innerText = ''
+            break;
+        case 'Del':
+            visor.innerText = visor.innerText.substr(0,visor.innerText.length-1)
+            break;
+        default:
+            visor.innerText += btn.innerText
+            break;
+    }
 }
